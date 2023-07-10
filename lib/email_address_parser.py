a311c2d3c319527ac4c8a42ea95a55f7b883333d
Eric Krukowski <erickruk@amazon.com>
2023-07-10 02:30:12 -0400

    Lab complete

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,11 @@
+import re
 # your code goes here!
+class EmailAddressParser:
+   def __init__(self, email_addresses):
+      self.all = email_addresses
+   def parse(self):
+      email_pattern = r"^[A-z][\w]*\.{0,1}[\w]+@[A-z]+.[A-z]+"
+      parse_pattern = r"[,]+\s{0,1}|\s{1}"
+      email_list = re.split(parse_pattern, self.all)
+      email_list = [email for email in email_list if re.fullmatch(email_pattern, email)]
+      return sorted(email_list)
